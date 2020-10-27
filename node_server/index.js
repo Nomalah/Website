ccec3c3cbbc3c97b381c5daeb26ff5faa88a3755
Nomalah <36043275+Nomalah@users.noreply.github.com>
2020-10-27 15:04:22 -0400

    Updated the home page

--- node_server/index.js
+++ node_server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/assets", express.static("../assets"))
+app.use("/", express.static("../"))
 
 app.get("/", (req, res) => {
     console.log("Request to homepage")
